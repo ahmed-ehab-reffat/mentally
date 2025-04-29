1e5dd4b89838767e7aa4dd7daf74cec610f748d5
ahmed-ehab-reffat <ahmedehab62003@gmail.com>
2025-04-29 04:19:18 +0300

    make the landing responsive

--- components/home/landing.tsx
+++ components/home/landing.tsx
@@ -4,16 +4,19 @@ import landing from "@/assets/images/landing.jpg";
 
 export default function Landing() {
   return (
-    <section id="landing" className="h-[calc(100dvh-4rem)] relative">
+    <section
+      id="landing"
+      className="h-[calc(40dvh-4rem)] sm:h-[calc(50dvh-4rem)] md:h-[calc(75dvh-4rem)] lg:h-[calc(100dvh-4rem)] relative"
+    >
       <Image
         src={landing}
         alt="wooden cubes with faces on them"
         quality={100}
-        sizes="100vh 100vw"
+        // sizes="(max-width: 640px) 40vh, (min-width: 640px) 50vh, (min-width: 768px) 75vh, (min-width: 1024px) 100vh"
         fill
       />
-      <div className="absolute text-5xl font-black italic text-center text-secondry top-30 left-25">
-        <h2 className="mb-6">Together</h2>
+      <div className="absolute font-black italic text-center text-secondry text-xl top-5 left-7 sm:text-3xl sm:top-12 sm:left-15 md:text-4xl md:top-25 md:left-20 lg:text-5xl lg:top-30 lg:left-30">
+        <h2 className="mb-1 sm:mb-2 md:mb-4 lg:mb-6">Together</h2>
         <h2>We Can Overcome</h2>
       </div>
     </section>
