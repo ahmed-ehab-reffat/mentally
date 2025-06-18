d0ff25cce6cf11875c7476241510993644600415
ahmed-ehab-reffat <ahmedehab62003@gmail.com>
2025-06-18 16:41:52 +0300

    move auth code to new branch

--- components/header.tsx
+++ components/header.tsx
@@ -91,9 +91,6 @@ export default function Header() {
           <Link href="#contactus" onClick={handleClose}>
             {t("contact")}
           </Link>
-          {/* <Link href="/auth" onClick={handleClose}>
-            {t("login")}
-          </Link> */}
           <div
             onClick={toggleTheme}
             className="cursor-pointer flex gap-2 items-center"
